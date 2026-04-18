e92a08934e06d27909df22990ae20c0813791268
Alexbigot5 <112185659+Alexbigot5@users.noreply.github.com>
2026-04-17 20:20:03 -0700

    Add migration005 to the migrations array

--- backend/runMigrations.js
+++ backend/runMigrations.js
@@ -5,8 +5,9 @@ const migration001 = require('./migrations/001_create_users_table');
 const migration002 = require('./migrations/002_create_preferences_table');
 const migration003 = require('./migrations/003_create_scrape_log');
 const migration004 = require('./migrations/004_add_onboarded_to_users');
+const migration005 = require('./migrations/005_add_google_email');
 
-const migrations = [migration001, migration002, migration003, migration004];
+const migrations = [migration001, migration002, migration003, migration004, migration005];
 
 async function runMigrations() {
   const pool = new Pool({
